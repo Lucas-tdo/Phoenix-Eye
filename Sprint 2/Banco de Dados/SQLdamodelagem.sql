64465260d1447a75d5b09a98841638bf4c625e75
Lucas-tdo <lucas.santos114@sptech.school>
2025-04-23 14:12:08 -0300

    Atualizando banco e voltando para o site claro no login/cadastro

--- Sprint 2/Banco de Dados/SQLdamodelagem.sql
+++ Sprint 2/Banco de Dados/SQLdamodelagem.sql
@@ -2,35 +2,38 @@ use sprint2;
 
 create table orgao(
 idOrgao int primary key auto_increment,
+orgao varchar(45),
 CNPJ char(15),
-email varchar(50),
-telefone char(11)
+telefone char(11),
+email varchar(45),
+senha varchar(45)
 );
 
 create table usuario(
-idUser int primary key auto_increment,
+idUser int auto_increment,
 Nome varchar(50),
 sobrenome varchar(50),
 email varchar(50),
+senha varchar(45),
 Cpf char(11),
-Nivel int,
 dtCadastro datetime default current_timestamp,
-fkOrganizacao int,
-constraint fkOrganizacao foreign key (fkOrganizacao) references orgao(idOrgao)
+FKOrgao int,
+constraint fkOrganizacao foreign key (fkOrgao) references orgao(idOrgao),
+primary key (idUser,FKOrgao)
 );
 
 create table Area(
 idArea int primary key auto_increment,
 Grid char(1),
-local int,
+numero int,
 fkOrgao int,
 constraint fkOrgao foreign key (fkOrgao) references orgao(idOrgao)
 );
 
 CREATE TABLE sensor (
-idSensor INT PRIMARY KEY AUTO_INCREMENT,
+idSensor int primary key AUTO_INCREMENT,
 nomeSensor VARCHAR(30),
-statusSensor VARCHAR(20),
+Status_Sensor VARCHAR(20),
 dtInstalacao DATETIME,
 dtManutencao DATETIME,
 descManutencao VARCHAR(500),
@@ -39,12 +42,15 @@ CONSTRAINT fkArea FOREIGN KEY (fkArea) REFERENCES Area(idArea)
 );
 
 CREATE TABLE dados (
-idDados INT PRIMARY KEY AUTO_INCREMENT,
+idDados INT AUTO_INCREMENT,
 temperatura DECIMAL(4,2),
 umidade INT,
 dtMedicao DATETIME,
 nivelRisco INT,
 fkSensor  INT,
-CONSTRAINT fkSensor FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
+CONSTRAINT fkSensor FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor),
+primary key (idDados,fkSensor)
 );
+
+
 
