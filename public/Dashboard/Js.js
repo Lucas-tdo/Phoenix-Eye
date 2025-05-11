76f440e196cd400ca49239b49da52043e189c358
GuiMarquesCode <guilherme.csantos@sptech.school>
2025-05-11 18:14:53 -0300

    Página do perfil

--- public/Dashboard/Js.js
+++ public/Dashboard/Js.js
@@ -1,5 +1,5 @@
-var tempSensorA7 = 58;
-var umidSensorA7 = 20;
+var tempSensorA7 = 50;
+var umidSensorA7 = 60;
 
 var umidAviso = 40;
 var tempAviso = 34;
@@ -23,5 +23,10 @@ var tempSA9 = tempSensorA7 - 6;
 var umidSA9 = umidSensorA7 + 6;
 
 
-tempMedia = (28 + 29 + 27 + 30 + tempSA5 + tempSA6 + tempSensorA7 + tempSA8 + tempSA9 + 27 + 30 + 26 + 29 + 27 + 30 + 27 + 30 + 27 + 30 + 27 + 30 + 27 + 30 + 28 + 29 + 27 + 30 + 26 + 25 + 27 + 28 + 29) /32;
-umidMedia = (70 + 72 + 68 + 75 + umidSA5 + umidSA6 + umidSensorA7 + umidSA8 + umidSA9 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 70 + 72 + 68 + 75 + 65 + 63 + 63 + 70 + 72) / 32;
+tempMedia = (28 + 29 + 27 + 30 + tempSA5 + tempSA6 + tempSensorA7 + tempSA8 + tempSA9 +
+27 + 30 + 26 + 29 + 27 + 30 + 27 + 30 + 27 + 30 + 27 + 30 + 27 + 30 + 28 + 29 + 27 + 30 + 26 + 25 + 27 + 28 + 29) /32;
+
+
+
+umidMedia = (70 + 72 + 68 + 75 + umidSA5 + umidSA6 + umidSensorA7 + umidSA8 + umidSA9 + 68
++ 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 68 + 75 + 70 + 72 + 68 + 75 + 65 + 63 + 63 + 70 + 72) / 32;
