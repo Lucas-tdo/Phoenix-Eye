c98d5ea7ef85252848711434a52531b8c95026e6
Lucas-tdo <lucas.santos114@sptech.school>
2025-06-05 12:43:57 -0300

    Arrumando alerta

--- Plataforma/public/dashboard/alerta.js
+++ Plataforma/public/dashboard/alerta.js
@@ -1,4 +1,5 @@
 var idOrgao = 1
+alerta.style.display=`none`
 
 async function obterdados() {
     const resp = await fetch(`/medidas/verificarAviso/${idOrgao}`);
@@ -8,31 +9,34 @@ async function obterdados() {
 
     var mensagem = ""
 
-
-
     if (resp_dados.length > 0) {
-        mensagem = `<div class='aviso' style = "position : absolute; display: flex; background-color: blue;">`
-
+        mensagem = ``
+        alerta.style.display=`block`
         for (var i = 0; i < resp_dados.length; i++) {
-
             mensagem += `
-            <div>
-            <h1>${resp_dados[i].nome}</h1>
-            <h1>${resp_dados[i].Situacao_dado}</h1>
-            <h1>${resp_dados[i].Nome_atribuido}</h1>
-            <h1>${resp_dados[i].Data}
-            </div>`;
-
-
+             <div id="${resp_dados[i].Data}">
+                <div>
+                    <img src="Assets/Icons/alerta.png" alt="">
+                    <img src="Assets/Icons/Recusado.png" onclick="limpar_notificacao('${resp_dados[i].Data}')" id="sair" alt="">
+                </div>
+                <h1>${resp_dados[i].nome}</h1>
+                <h1>${resp_dados[i].Situacao_dado}</h1>
+                <h1>${resp_dados[i].Nome_Atribuido}</h1>
+                <h1>${resp_dados[i].Data}</h1>
+            </div>
+            `;
         }
-        mensagem += "</div>"
-
-
+        alerta.innerHTML+=mensagem
        
     }
-
-
-
 }
 
-obterdados()
+     function limpar_notificacao(id){
+    var div = document.getElementById(`${id}`)
+    div.remove()
+    if(alerta.innerHTML == ``){
+        alerta.style.display=`none`
+    }
+}
+
+    obterdados()
